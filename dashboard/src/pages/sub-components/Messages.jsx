1c60d0cdcc21bae4bcf0107dde204c30d32f3377
Vikas Rathore <149600680+Vikas123Rathore@users.noreply.github.com>
2026-02-01 13:50:54 +0530

    Fix API URL in fetchMessages function

--- dashboard/src/pages/sub-components/Messages.jsx
+++ dashboard/src/pages/sub-components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
   const fetchMessages = async () => {
     try {
       const { data } = await axios.get(
-        "https://my-3d-portfolio-w2c6.onrender.comapi/v1/message/getall",
+        "https://my-3d-portfolio-w2c6.onrender.com/api/v1/message/getall",
         { withCredentials: true }
       );
       setMessages(data.messages);
