b3dee5db25fddd84fda9f5b84f19114f719059c2
Vikas Rathore <149600680+Vikas123Rathore@users.noreply.github.com>
2026-02-01 13:02:35 +0530

    Update API endpoint for fetching messages

--- dashboard/src/pages/sub-components/Messages.jsx
+++ dashboard/src/pages/sub-components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
   const fetchMessages = async () => {
     try {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/message/getall",
+        "https://my-3d-portfolio-w2c6.onrender.comapi/v1/message/getall",
         { withCredentials: true }
       );
       setMessages(data.messages);
